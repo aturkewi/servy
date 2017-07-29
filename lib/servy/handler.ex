b777819c794db1d8f00eeb7c513f4c8223f8fee0
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-07-29 17:22:33 -0500

    Build Handler.handle pipeline

---
+++ lib/servy/handler.ex
@@ -0,0 +1,44 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    # conv = parse(request)
+    # conv = route(conv)
+    # format_response(conv)
+    
+    # lo mismo
+    
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+  
+  def parse(request) do
+    conv = %{ method: "GET", path: "/wildthings", resp_body: ""}
+  end
+  
+  def route(conv) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+  
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
+
+request = """
+GET /wilthins HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
