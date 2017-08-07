75aec1b5de719ded92867ff6eb46c6e471920d42
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-08-07 18:49:50 -0400

    Returning dynamic response

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -20,29 +20,28 @@ defmodule Servy.Handler do
       request
       |> String.split("\n")
       |> List.first
-      String.split(" ")
-      [method, path, _]
+      |> String.split(" ")
       
     %{ method: method, path: path, resp_body: ""}
   end
   
   def route(conv) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+    %{conv | resp_body: "Bears, Lions, Tigers"}
   end
   
   def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
 
 request = """
-GET /wildthins HTTP/1.1
+GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
